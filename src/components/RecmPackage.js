4309e2ff238d800b698f791a702afdf26025d8ed
Bahareh Bakhtiari <bahareh4@gmail.com>
2022-06-12 15:37:46 +0430

    change some codes

--- src/components/RecmPackage.js
+++ src/components/RecmPackage.js
@@ -1,3 +1,4 @@
+import React from 'react'
 
 function RecmPackage() {
   const Packages = [
@@ -5,25 +6,48 @@ function RecmPackage() {
       ExpDate: '4 شهریور 1401',
       PackageName: 'بسته اینترنت سه ماهه',
       Discount: '20%',
-      Price: '130000'
+      Price: '130,000'
     },
     {
       ExpDate: '12 خرداد 1401',
       PackageName: 'بسته ترکیبی هفتگی',
       Discount: '45%',
-      Price: '45000'
+      Price: '45,000'
     },
     {
-      ExpDate: '15تیر 1401',
+      ExpDate: '15 تیر 1401',
       PackageName: 'بسته اینترنت یکماهه',
       Discount: '15%',
-      Price: '50000'
+      Price: '50,000'
     },
   ]
   return (
     <div className='rcmpackage'>
-      <p dir='rtl'>بسته های پیشنهادی مخصوص خود شما!</p>
-      <RecmPackage Packages={Packages} />
+      <h1 style={{ float: 'right', padding: '0 20px 0 16px', fontSize: 20 }}>
+      بسته های پیشنهادی مخصوص خود شما!</h1>
+      <table dir='rtl'>
+        <tr style={{ textAlign: 'right', marginRight: 33 }}>
+          <th style={{ paddingRight: 33 }}>تاریخ انقضا بسته</th>
+          <th>نام بسته</th>
+          <th>تخفیف</th>
+          <th>قیمت</th>
+          <th></th>
+        </tr> 
+        <tbody>
+          {mockRowData.map((item) => (
+            <tr>
+              <td style={{ paddingRight: 33 }}>{item.date}</td>
+              <td>{item.PackageName}</td>
+              <td>{item.Discount}</td>
+              <td>{item.Price}</td>
+              <td>
+                <button className="buy-button">خرید</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>       
+      </table>
+      {/* <RecmPackage Packages={Packages} /> */}
     </div>
   )
 }
