9b359f388c3cbde3ce0b4c3df2d1d1e58d767a8c
paria <104171012+PARIA-NAJAFI@users.noreply.github.com>
2022-06-15 21:11:39 +0430

    app edit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react'
 import MiddleBar from './components/MiddleBar';
 import Menu from './components/Menu';
 import LeftBar from './components/LeftBar';
+import ConsumChart from './components/ConsumChart';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <MiddleBar/>
         <LeftBar/>
       </div>
+
     </div>
   )
 }
