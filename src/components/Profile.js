9574e40232ed1b0e15cb37768ef2ece8e935eb45
paria <104171012+PARIA-NAJAFI@users.noreply.github.com>
2022-06-15 19:15:58 +0430

    profile commit

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,11 +4,12 @@ import CustomerImg from '../assets/images/customer.png'
 function Profile() {
   return (
     <div className='profile'>
+      <div className='txt-p'>
       <section>
         <h3>سلام، بهاره بختیاری </h3>
         <p>+98 9360011223</p>
-      </section>
-      <img src={CustomerImg} alt="CustomerImage" width={60} height={60} />
+      </section></div>
+      <img className='img-p' src={CustomerImg} alt="CustomerImage" width={60} height={60} />
     </div>
   )
 }
